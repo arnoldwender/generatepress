70c6a9f31fcdd2d8715695381d65f1fc29b82c2b
Arnold Wender <arnold.wender@gmail.com>
2025-11-30 08:55:49 +0100

    Update modal asset version and optimize modal.js code structure

--- assets/dist/block-editor.asset.php
+++ assets/dist/block-editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-data', 'wp-dom-ready', 'wp-editor', 'wp-element', 'wp-plugins'), 'version' => '24bb9110ccf231b1e49f');
+<?php return array('dependencies' => array('wp-data', 'wp-dom-ready', 'wp-editor', 'wp-element', 'wp-plugins'), 'version' => 'd2f6c565754717c98f6a');
--- assets/dist/modal.asset.php
+++ assets/dist/modal.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => 'b9041bb3f75d06782df7');
+<?php return array('dependencies' => array(), 'version' => '628dd290e0ab9512f218');
